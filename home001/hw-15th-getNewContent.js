31298050dfe4f5f53a9e43a0e1f8fc7b2bddd61e
LinKding <Linkding@linxiaobindeMacBook.local>
2017-01-14 09:01:24 +0800

    update detail  15th  001

---
+++ home001/hw-15th-getNewContent.js
@@ -0,0 +1,18 @@
+function getNewContent(){
+  var request = getHTTPObject();
+  if (request){
+    request.open("GET", "hw-15th.txt", true);
+    request.onreadstatechange = function() {
+      if (request.readState == 4) {
+      var para = document.createElement("p");
+      var text = document.createTextNode(request.resposeText);
+      para.appendChild(txt);
+      document.getElementById('new').appendChild(para);
+      }
+    };
+    request.send(null);
+  }
+  else{
+    alert('Sorry,your brower doesn\'t support XMLHttpRequest');
+  }
+}
