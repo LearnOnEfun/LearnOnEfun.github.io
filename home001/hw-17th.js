2a08aa2927a1047f0b346ee232bca5e2712743f5
LinKding <beninaoin@gmail.com>
2017-02-28 11:36:36 +0800

    update 17th   完成画布

--- home001/hw-17th.js
+++ home001/hw-17th.js
@@ -1,7 +1,25 @@
 //画布
-function plot(){
+window.onload = function plot(){
+  var canvas = document.querySelector("#canvas")
+  ctx = canvas.getContext('2d');
+  cW = canvas.width;
+  cH = canvas.height;
+  len = 100;
+  w = cW / len ;
+  h = cH / len ;
 
+  function drawline(x1,y1,x2,y2){
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.stroke();
+  }
+
+  for (var i = 0; i <= len; i++){
+    drawline(w * i, 0, w * i, cH)
+    drawline(0, h * i, cW, h * i)
+  }
 }
+
 
 function draw(){
 
